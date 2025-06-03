6eb43b2291f6351e68046c173ec85d2bbc36d962
Darrel Malkin <catrassyt@gmail.com>
2025-06-03 19:23:58 +1000

    fix: fix click detection and state not updating when refreshing matrix

--- src/script/main.js
+++ src/script/main.js
@@ -4,15 +4,19 @@ function rand(min, max) {
 
 var pool = ["E9", "BD", "1C", "55", "7A"];
 
+// Function to generate code and click matrix
 function generateMatrix() {
   var matrix = [];
 
   for (let row = 0; row < 6; row++) {
     matrix.push([]);
+    // In each index of value, we will have the matrix code, and that
+    // cells 'click state'.
+    // So one cell will look like: ["E9", "clickedFalse"]
     for (let col = 0; col < 6; col++) {
       randomIndex = rand(0, pool.length);
       randomVal = pool[randomIndex];
-      matrix[row].push(randomVal);
+      matrix[row].push([randomVal, "clickedFalse"]);
     }
   }
 
@@ -40,5 +44,5 @@ function generateCodes() {
 }
 
 function clickedCode(matrixVal) {
-  console.log("Clicked:", matrixVal);
+  console.log("Clicked:", matrixVal[0]);
 }
